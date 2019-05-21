ef867f3ca34638e6ab4d1a475082b36ac61c6636
SilveKnife <z516798599@qq.com>
2019-05-21 15:42:39 +0800

    update

--- app/src/main/java/com/silverknife/meizhi/MainActivity.kt
+++ app/src/main/java/com/silverknife/meizhi/MainActivity.kt
@@ -1,35 +1,13 @@
 package com.silverknife.meizhi
 
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        progress_circular.setShowText(true)
-        progress_circular.setTextColor(Color.parseColor("#000000"))
-        progress_circular.setOnProgressListener { progress -> seekbar.setProgress(progress.toInt()) }
-        progress_circular.setDuration(4000)
-        progress_circular.startAnime()
-        seekbar.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
 
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-            }
-
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                println(progress)
-                progress_circular.setProgress(progress)
-            }
-
-        })
     }
 }
