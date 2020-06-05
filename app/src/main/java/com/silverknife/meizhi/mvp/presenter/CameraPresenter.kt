d1025c985c321b3331b6d87adc694f957b4391e9
SilverKnife <z516798599@gmail.com>
2020-06-05 10:31:17 +0800

    添加相机

---
+++ app/src/main/java/com/silverknife/meizhi/mvp/presenter/CameraPresenter.kt
@@ -0,0 +1,9 @@
+package com.silverknife.meizhi.mvp.presenter
+
+import com.silvericekey.skutilslibrary.base.BasePresenter
+
+class CameraPresenter:BasePresenter() {
+    override fun onDestroy() {
+
+    }
+}
