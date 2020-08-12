c32657a008badcf0674ca485efd925afece6253a
SilverIceKey <z516798599@gmail.com>
2020-08-12 13:53:09 +0800

    realm init

--- SKLibrary/src/main/java/com/silvericekey/skutilslibrary/base/BaseApplication.kt
+++ SKLibrary/src/main/java/com/silvericekey/skutilslibrary/base/BaseApplication.kt
@@ -2,6 +2,8 @@ package com.silvericekey.skutilslibrary.base
 
 import androidx.multidex.MultiDexApplication
 import com.silvericekey.skutilslibrary.SKUtilsLibrary
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 open class BaseApplication : MultiDexApplication() {
     companion object {
@@ -16,6 +18,12 @@ open class BaseApplication : MultiDexApplication() {
         super.onCreate()
         app = this
         SKUtilsLibrary.init(app!!)
+        Realm.init(this)
+        Realm.setDefaultConfiguration(
+                RealmConfiguration.Builder().name("realmData")
+                        .deleteRealmIfMigrationNeeded()
+                        .build()
+        )
     }
 
     open fun statusChange() {
